e3bfdacb7a35aff20040bdbb8309612c9ba889d3
IgnjatJokanovic <ignjat.jokanovic.311.16@ict.edu.rs>
2019-02-17 23:06:38 +0100

    Finished actor views

--- models/Actor.php
+++ models/Actor.php
@@ -5,10 +5,13 @@
     use models\DB;
     class Actor
     {
+        public $id;
         public $name;
         public $surname;
         public $description;
         public $image;
+        public $alt;
+        public $rating;
         public $galery = array();
 
         public function save()
@@ -47,6 +50,27 @@
             $con = DB::getInstance()->getConnection();
             return $con->query("select * from actor")->fetchAll();
         }
+
+        public function paginate($limit, $offset)
+        {
+            $con = DB::getInstance()->getConnection();
+            return $con->query("select a.id as id, a.name as name, a.surname as surname, i.src as image, i.alt as alt from actor a join image i on a.img_id=i.id limit $limit, $offset")->fetchAll();
+
+        }
+        public function findById($id)
+        {
+            $con = DB::getInstance()->getConnection();
+            $actor = $con->query("select a.id as id, a.name as name, a.surname as surname, a.description as description, i.src as image, i.alt as alt from actor a join image i on a.img_id=i.id where a.id=$id")->fetch();
+            $this->id = $actor->id;
+            $this->name = $actor->name;
+            $this->surname = $actor->surname;
+            $this->description =  $actor->description;
+            $this->image = $actor->image;
+            $this->galery = $con->query("select i.src, i.alt from image i join actor_galery ag on i.id=ag.id_img where ag.id_actor=$id")->fetchAll();
+            $this->rating = $con->query("select avg(rating) as avg from actor_rating where id_actor=$id")->fetch();
+        }
+
+
 
     }
 
